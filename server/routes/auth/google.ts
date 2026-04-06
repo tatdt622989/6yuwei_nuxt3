e12aa4edd74c77f2e41cc368bad09aa6352baca9
劉育瑋 <>
2026-04-06 19:58:20 +0800

    feat: update Google authentication to use runtime configuration for API proxy target

--- server/routes/auth/google.ts
+++ server/routes/auth/google.ts
@@ -1,5 +1,6 @@
 export default defineEventHandler(async (event) => {
-  const target = process.env.NUXT_API_PROXY_TARGET || 'http://localhost:3001';
+  const { apiProxyTarget } = useRuntimeConfig();
+  const target = apiProxyTarget || 'http://localhost:3001';
   const res = await fetch(`${target}/googleLogin/`, { redirect: 'manual' });
   const location = res.headers.get('location');
   if (location) {
